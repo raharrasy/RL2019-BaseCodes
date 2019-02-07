230f7d89cd7857ca6e832e3b7a3fae2b6ca65d27
raharrasy <rahmanarrasy@gmail.com>
2019-02-07 14:03:53 +0000

    Remove unnecessary lines from Monte Carlo sh

--- Task2/MonteCarlo/MonteCarlo.sh
+++ Task2/MonteCarlo/MonteCarlo.sh
@@ -4,24 +4,23 @@
 # Cannot run an environment where defending agents exist but none are playing
 # goalkeeper
 
-./../../../bin/HFO --defense-agents=2 --offense-agents=1 --offense-on-ball 11 --trials 4000 --headless --deterministic --frames-per-trial 2000 --untouched-time 2000 &
+./../../../bin/HFO --defense-agents=2 --offense-agents=1 --offense-on-ball 11 --trials 500 --headless --deterministic --discrete=True --frames-per-trial 2000 --untouched-time 2000 &
 sleep 5
-./DiscreteHFO/Initiator.py --numTrials=4000 --numPlayingDefenseNPCs=1 --numAgents=1 &
+./DiscreteHFO/Initiator.py --numTrials=500 --numPlayingDefenseNPCs=1 --numAgents=1 &
 echo "Environment Initialized"
+
 # Sleep is needed to make sure doesn't get connected too soon, as unum 1 (goalie)
+sleep 5
+
+./MonteCarloBase.py --numEpisodes=500 --numOpponents=1 &
+echo "Attacker Controller Initialized"
 
 sleep 5
-./MonteCarloAgent2.py --numEpisodes=4000 --numOpponents=1 &
-# Remove the line above with the line below when grading
-#./MonteCarloUnitTest.py &
-#echo "Attacker Controller Initialized"
-
-sleep 5
-./DiscreteHFO/Goalkeeper.py --numEpisodes=4000 &
+./DiscreteHFO/Goalkeeper.py --numEpisodes=500 &
 echo "Goalkeeper Initialized"
 
 sleep 5
-./DiscreteHFO/DiscretizedDefendingPlayer.py --id=1 --numEpisodes=4000 &
+./DiscreteHFO/DiscretizedDefendingPlayer.py --id=1 --numEpisodes=500 &
 echo "Defending Player Initialized"
 
 sleep 5
