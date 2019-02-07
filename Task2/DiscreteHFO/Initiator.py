bfa058e6fe8b48141aedeb5c2ce835e074e23187
raharrasy <rahmanarrasy@gmail.com>
2019-02-07 13:56:27 +0000

    Remove randomizing starts in init

--- Task2/DiscreteHFO/Initiator.py
+++ Task2/DiscreteHFO/Initiator.py
@@ -20,23 +20,9 @@ if __name__ == '__main__':
 	numAgents = args.numAgents
 
 	f = open("initCoordinates.txt", "w")
-	possibleLocs = [[0]*5] * 5
-	allocatedLocs = numDefenseNPCs + numAgents
-	locs = []
-	while allocatedLocs > 0:
-		coordX = random.randint(0,4)
-		coordY = random.randint(0,4)
-		if possibleLocs[coordX][coordY] == 0:
-			possibleLocs[coordX][coordY] = 1
-			locs.append([coordX,coordY])
-			allocatedLocs -= 1
-
 	locs = [[0,2],[2,2]]
 
 	for epsNumber in range(numTrials):
-                #possibleLocs = [[0]*5] * 5
-                #allocatedLocs = numDefenseNPCs + numAgents
-                #locs = []
 		f.write(str(locs)+'\n')
 
 	f.close()
