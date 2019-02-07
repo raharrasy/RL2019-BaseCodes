22ebc997e95b35ed7c16af2cadfdb28f349fb433
raharrasy <rahmanarrasy@gmail.com>
2019-02-07 12:34:42 +0000

    Add kick probabilities for last row

--- Task2/DiscreteHFO/EnvironmentDynamics.py
+++ Task2/DiscreteHFO/EnvironmentDynamics.py
@@ -8,7 +8,7 @@ class Dynamics(object):
 								"DRIBBLE_RIGHT":{"DRIBBLE_UP" : 0.05, "DRIBBLE_DOWN":0.05, "DRIBBLE_LEFT":0.05, "DRIBBLE_RIGHT":0.85}}
 
 
-		self.kickDynamics = [[0.05,0.08,0.1,0.08,0.05],[0.05, 0.1,0.2,0.1,0.05],[0.15,0.25,0.35,0.25,0.15],[0.2,0.35,0.6,0.35,0.2],[0.35,0.7,0.9,0.7,0.35]]
+		self.kickDynamics = [[0.05,0.08,0.1,0.1,0.08,0.05],[0.05, 0.1,0.2,0.2,0.1,0.05],[0.15,0.25,0.35,0.35,0.25,0.15],[0.2,0.35,0.6,0.6,0.35,0.2],[0.35,0.7,0.9,0.9,0.7,0.35]]
 	
 	def setDribbleDynamics(self, dynamics):
 		self.dribbleDynamics = dynamics
