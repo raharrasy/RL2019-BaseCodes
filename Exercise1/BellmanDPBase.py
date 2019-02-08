ee1688038c3da4e7793ac1560c5373c54ded5109
raharrasy <rahmanarrasy@gmail.com>
2019-02-08 08:26:11 +0000

    Update BellmanDPBase.py

--- Exercise1/BellmanDPBase.py
+++ Exercise1/BellmanDPBase.py
@@ -9,4 +9,11 @@ class BellmanDPSolver(object):
 	def BellmanUpdate():
 
 		raise NotImplementedError
+		
+if __name__ == '__main__':
+	solution = BellmanDPSolver()
+	for i in range(20000):
+		values, policy = solution.BellmanUpdate()
+	print("Values : ", values)
+	print("Policy : ", policy)
 
