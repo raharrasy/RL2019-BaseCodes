3fecb6610d63d9a4410d06969d4a991a39c5055d
Muhammad Arrasy Rahman <rahmanarrasy@gmail.com>
2019-03-03 10:24:46 +0000

    Fix missing Function in WolfPHCBase.py

--- Exercise4/WolfPHCAgent/WolfPHCBase.py
+++ Exercise4/WolfPHCAgent/WolfPHCBase.py
@@ -9,42 +9,63 @@ from copy import deepcopy
 import numpy as np
 		
 class WolfPHCAgent(Agent):
-	def __init__(self, learningRate, discountFactor, initVals, winDelta, loseDelta):
+	def __init__(self, learningRate, discountFactor, winDelta=0.01, loseDelta=0.1, initVals=0.0):
 		super(WolfPHCAgent, self).__init__()
 		
-
+		
 	def setExperience(self, state, action, reward, status, nextState):
+		raise NotImplementedError
 
 	def learn(self):
+		raise NotImplementedError
 
 	def act(self):
+		raise NotImplementedError
 
 	def calculateAveragePolicyUpdate(self):
+		raise NotImplementedError
 
 	def calculatePolicyUpdate(self):
+		raise NotImplementedError
 
+	
 	def toStateRepresentation(self, state):
+		raise NotImplementedError
 
 	def setState(self, state):
+		raise NotImplementedError
+
+	def setLearningRate(self,lr):
+		raise NotImplementedError
 		
 	def setWinDelta(self, winDelta):
+		raise NotImplementedError
 		
 	def setLoseDelta(self, loseDelta):
+		raise NotImplementedError
 	
 	def computeHyperparameters(self, numTakenActions, episodeNumber):
+		raise NotImplementedError
 
 if __name__ == '__main__':
 
-	numOpponents = 1
-	numAgents = 2
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--numOpponents', type=int, default=1)
+	parser.add_argument('--numAgents', type=int, default=2)
+	parser.add_argument('--numEpisodes', type=int, default=100000)
+
+	args=parser.parse_args()
+
+	numOpponents = args.numOpponents
+	numAgents = args.numAgents
 	MARLEnv = DiscreteMARLEnvironment(numOpponents = numOpponents, numAgents = numAgents)
 
 	agents = []
 	for i in range(args.numAgents):
-		agent = WolfPHCAgent(learningRate = 0.2, discountFactor = 0.99)
+		agent = WolfPHCAgent(learningRate = 0.2, discountFactor = 0.99, winDelta=0.01, loseDelta=0.1)
 		agents.append(agent)
 
-	numEpisodes = 4000
+	numEpisodes = args.numEpisodes
 	numTakenActions = 0
 	for episode in range(numEpisodes):	
 		status = ["IN_GAME","IN_GAME","IN_GAME"]
