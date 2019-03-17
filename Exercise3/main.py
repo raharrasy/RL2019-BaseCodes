9f2aebfc7d7a5ae8f83826e780353abc24deff35
raharrasy <rahmanarrasy@gmail.com>
2019-03-17 11:38:00 +0000

    Update main.py

--- Exercise3/main.py
+++ Exercise3/main.py
@@ -4,23 +4,24 @@
 
 # Use this script to handle arguments and 
 # initialize important components of your experiment.
-# These might include important parameters for your experiment,
+# These might include important parameters for your experiment, and initialization of
 # your models, torch's multiprocessing methods, etc.
 if __name__ == "__main__" :
 	
 	# Example on how to initialize global locks for processes
 	# and counters.
 	
-	counter = mp.Value('i', 0)
-	lock = mp.Lock()
+	#counter = mp.Value('i', 0)
+	#lock = mp.Lock()
 
-	for idx in range(0, args.num_processes):
-		trainingArgs = (idx, args, value_network, target_value_network, optimizer, lock, counter)
-		p = mp.Process(target=train, args=())
-		p.start()
-		processes.append(p)
-	for p in processes:
-		p.join()
+	# Example code to initialize torch multiprocessing.
+	#for idx in range(0, args.num_processes):
+	#	trainingArgs = (idx, args, value_network, target_value_network, optimizer, lock, counter)
+	#	p = mp.Process(target=train, args=())
+	#	p.start()
+	#	processes.append(p)
+	#for p in processes:
+	#	p.join()
 
 
 
